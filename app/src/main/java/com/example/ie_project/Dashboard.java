9c8267b5bce19897abf9ebdc93e7e9b510bc6c33
shuaibiyige <dashuaibiyige@gmail.com>
2019-09-07 17:15:26 +1000

    commit30

--- app/src/main/java/com/example/ie_project/Dashboard.java
+++ app/src/main/java/com/example/ie_project/Dashboard.java
@@ -54,9 +54,9 @@ public class Dashboard extends Fragment
         linearLayout.startAnimation(ani1);
         man_image.startAnimation(ani2);
 
-        int[] list = size();
-        int width = list[0];
-        int height = list[1];
+//        int[] list = size();
+//        int width = list[0];
+//        int height = list[1];
 //        man_image.getLayoutParams().height = height;
 //        man_image.getLayoutParams().width = width;
 //        man_image.requestLayout();
--- app/src/main/java/com/example/ie_project/Questionnaire1.java
+++ app/src/main/java/com/example/ie_project/Questionnaire1.java
@@ -2,16 +2,24 @@ package com.example.ie_project;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daimajia.numberprogressbar.NumberProgressBar;
@@ -20,16 +28,16 @@ import com.ramotion.foldingcell.FoldingCell;
 
 public class Questionnaire1 extends Fragment
 {
+    private static final String TAG = "aaaaaa";
     View question1;
-    private Button next;
-    private NumberProgressBar bnp;
-    private RadioGroup radioGroup;
-    private RadioButton radioButton;
-    private int checkedNumber;
+    private Button submit;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         question1 = inflater.inflate(R.layout.questionnaire1_layout, container, false);
+
+        final FragmentManager fragmentManager = getFragmentManager();
+
 //        bnp = (NumberProgressBar) question1.findViewById(R.id.progress_bar1);
 //        next = (Button) question1.findViewById(R.id.next_button1);
 //        radioGroup = (RadioGroup) question1.findViewById(R.id.radioGroup_gender);
@@ -57,7 +65,32 @@ public class Questionnaire1 extends Fragment
 //            }
 //        });
 
+//        getScreenSizeOfDevice2();
+//        ViewGroup.LayoutParams lp;
+//        LinearLayout l = question1.findViewById(R.id.l);
+//        lp = l.getLayoutParams();
+//        lp.width = (int)Math.round((5.24/5.14) * (l.getLayoutParams().width));
+//        Log.d(TAG, "line: " + l.getLayoutParams().width);
+
+
+        submit = (Button) question1.findViewById(R.id.submit_ques);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().replace(R.id.content_frame, new Successful()).commit();            }
+        });
 
         return question1;
     }
+
+    private void getScreenSizeOfDevice2() {
+        Point point = new Point();
+        getActivity().getWindowManager().getDefaultDisplay().getRealSize(point);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        double x = Math.pow(point.x/ dm.xdpi, 2);
+        double y = Math.pow(point.y / dm.ydpi, 2);
+        double screenInches = Math.sqrt(x + y);
+        Log.d(TAG, "Screen inches : " + screenInches);
+    }
+
 }
