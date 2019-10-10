a501567ee43480105f891cacc2cc6758df8e2bed
shuaibiyige <dashuaibiyige@gmail.com>
2019-10-10 12:59:35 +1100

    Lee final commit11

--- app/src/main/java/com/example/ie_project/Startup.java
+++ app/src/main/java/com/example/ie_project/Startup.java
@@ -2,16 +2,19 @@ package com.example.ie_project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Startup extends AppCompatActivity
 {
     private ImageView imageView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,18 +22,20 @@ public class Startup extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startup_page);
 
-        imageView = (ImageView) findViewById(R.id.logo);
+        imageView = findViewById(R.id.logo);
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.INVISIBLE);
 
         AnimationSet animationSet = new AnimationSet(true);
         AlphaAnimation alphaAnimation = new AlphaAnimation(0,1);
-        alphaAnimation.setDuration(1000);
+        alphaAnimation.setDuration(2500);
         animationSet.addAnimation(alphaAnimation);
         imageView.startAnimation(alphaAnimation);
         alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation)
             {
-                //t.setBackgroundResource(R.drawable.1);
+                progressBar.setVisibility(View.VISIBLE);
             }
 
             @Override
