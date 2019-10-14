7f400c7770dfba2bdb6cea5e282b318da2b78c67
shuaibiyige <dashuaibiyige@gmail.com>
2019-10-14 15:12:23 +1100

    Lee final commit17

--- app/src/main/java/com/example/ie_project/JourneyIdentification.java
+++ app/src/main/java/com/example/ie_project/JourneyIdentification.java
@@ -2,16 +2,28 @@ package com.example.ie_project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class JourneyIdentification extends AppCompatActivity implements View.OnClickListener {
+public class JourneyIdentification extends AppCompatActivity implements View.OnClickListener
+{
     private Button back;
+    private TextView textView;
+    private final static String HTML_TEXT = "<p><strong>What is cohesion?</strong></p>\n" +
+            "<p>Cohesion evaluates what is perceived as parental emotional warmth and bonding, intimacy, mutual support, care and reliability. Cohesion is deemed to be a core construct of the parent-child relationship.</p>\n" +
+            "<p><strong>What to do to improve cohesion?</strong></p>\n" +
+            "<p>Embed a sense of stability with each other by means of simple practices. Consistent implementation of such practices can induce better quality bonding and help the child open-up more frequently and/or willingly.</p>\n" +
+            "<p><strong>Examples of practices that can improve cohesion:</strong></p>\n" +
+            "<p>1. Having regular meals together</p>\n" +
+            "<p>2. Establishing daily/weekly/monthly family rituals such as movie nights, spa evenings, biking/walking exploration tours etc...</p>\n" +
+            "<p>3. Actively expressing interest in your child’s life and volunteering to share information about yours to illustrate genuine willingness to be part of each other’s life (without judgement)</p>";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,10 +33,10 @@ public class JourneyIdentification extends AppCompatActivity implements View.OnC
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-
-
-
+        textView = findViewById(R.id.journey_identification_text);
         back = findViewById(R.id.journey_identification_back);
+
+        textView.setText(Html.fromHtml(HTML_TEXT));
 
         back.setOnClickListener(this);
     }
